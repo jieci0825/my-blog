8f927ccca15b46de71959c05f925f00f8661b707
coderjc <2740954762@qq.com>
2024-08-01 16:03:48 +0800

    feat: 完成博客分类模块

--- blog-admin/src/apis/modules/blog-category/type.ts
+++ blog-admin/src/apis/modules/blog-category/type.ts
@@ -2,3 +2,7 @@ export interface BlogCategoryItem {
 	id: number
 	categoryName: string
 }
+
+export interface CreateBlogCategoryParams {
+	categoryName: string
+}
--- blog-admin/src/views/blog/blog-category/index.vue
+++ blog-admin/src/views/blog/blog-category/index.vue
@@ -1,22 +1,85 @@
 <script setup lang="ts">
 import categoryFormConfig from './config/tag-form.config'
 import categoryTableConfig from './config/tag-table.config'
+import { blogCategoryApi } from '@/apis'
+import { ActionType } from './types'
+import { ref } from 'vue'
+import { useRefs } from '@/hooks/use-refs'
+import { openDeleteMessageBox } from '@/utils'
+import type { BlogCategoryItem, CreateBlogCategoryParams } from '@/apis/modules/blog-category/type'
 
-const handleCreateCategory = () => {}
-const handleTableEdit = () => {}
+const diaglogTitle = ref('')
+const dialogVisable = ref(false)
+const curAction = ref<ActionType>(ActionType.CREATE)
+const curCategoryInfo = ref<BlogCategoryItem | CreateBlogCategoryParams>()
+const modeTitleMap = {
+	[ActionType.CREATE]: '创建分类',
+	[ActionType.EDIT]: '编辑分类'
+}
+
+function setInfo(row: BlogCategoryItem | null, mode: ActionType, isVisable: boolean = true) {
+	curCategoryInfo.value = row ? { ...row } : ({} as BlogCategoryItem)
+	curAction.value = mode
+	diaglogTitle.value = modeTitleMap[mode]
+	dialogVisable.value = isVisable
+}
+
+const handleCreateCategory = () => {
+	setInfo(null, ActionType.CREATE)
+}
+
+const handleTableEdit = (row: BlogCategoryItem) => {
+	setInfo(row, ActionType.EDIT)
+}
+
+const handleTableDelete = async (row: BlogCategoryItem) => {
+	await openDeleteMessageBox()
+	const resp = await blogCategoryApi.reqDeleteBlogCategory(row.id)
+	ElMessage.success(resp.msg)
+	refreshData()
+}
+
+const { refs, setRef } = useRefs()
+
+const handleSubmit = async (data: BlogCategoryItem) => {
+	let resp: any = undefined
+	if (curAction.value === ActionType.CREATE) {
+		resp = await blogCategoryApi.reqCreateBlogCategory(data)
+	} else {
+		resp = await blogCategoryApi.reqEditBlogCategory(data)
+	}
+	ElMessage.success(resp.msg)
+	dialogVisable.value = false
+	refreshData()
+}
+
+function refreshData() {
+	refs.categoryPageContentRef?.fetchData()
+}
 
 const usePageContent = {
-	// request:
+	request: blogCategoryApi.reqGetBlogCategoryList
 }
 </script>
 
 <template>
 	<div class="blog-category-container container">
 		<PageContent
+			:ref="setRef('categoryPageContentRef')"
 			@handleTableEdit="handleTableEdit"
+			@handleTableDelete="handleTableDelete"
 			@actCreate="handleCreateCategory"
 			:use-page-content="usePageContent"
 			:tableConfig="categoryTableConfig"></PageContent>
+
+		<JcDialog
+			v-model="dialogVisable"
+			:title="diaglogTitle">
+			<JcForm
+				@submit="handleSubmit"
+				v-model="curCategoryInfo"
+				v-bind="categoryFormConfig"></JcForm>
+		</JcDialog>
 	</div>
 </template>
 
